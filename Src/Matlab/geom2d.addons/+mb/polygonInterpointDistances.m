f1205a92c29d01e7de290764649b78e54a8c6d83
Nicolaj Kirchhof <nicolaj.kirchhof@gmail.com>
2014-05-18 02:54:36 +0200

    updated ip dist

--- Src/Matlab/geom2d.addons/+mb/polygonInterpointDistances.m
+++ Src/Matlab/geom2d.addons/+mb/polygonInterpointDistances.m
@@ -2,4 +2,4 @@ function [distances, pointcombinations] = polygonInterpointDistances(bpoly)
 
     bpoly_double = cell2mat(mb.flattenPolygon(bpoly));
     pointcombinations = comb2unique(1:size(bpoly_double,2)-1);
-    distances = sqrt(sum((bpoly_double(:,pointcombinations(:,1))-bpoly_double(:,pointcombinations(:,2))).^2, 1));
+    distances = sqrt(double(sum((bpoly_double(:,pointcombinations(:,1))-bpoly_double(:,pointcombinations(:,2))).^2, 1)));
