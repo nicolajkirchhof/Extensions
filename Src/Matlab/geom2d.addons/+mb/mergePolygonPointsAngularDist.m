60196f2434a34081cf73fc0aea762951ff0073dd
Nicolaj Kirchhof <nicolaj.kirchhof@gmail.com>
2014-06-10 23:32:45 +0200

    Fixed lot of logical errors in angular merge

--- Src/Matlab/geom2d.addons/+mb/mergePolygonPointsAngularDist.m
+++ Src/Matlab/geom2d.addons/+mb/mergePolygonPointsAngularDist.m
@@ -4,52 +4,120 @@ function [poly] = mergePolygonPointsAngularDist(poly, phi, center)
 % point of the poly. By default center is the first point.
 
 
-if nargin < 3
+% if nargin > 2
     %%
     point_center = int64(center(1:2));
-    is_center = (poly(1,1:end-1)==center(1))&(poly(2,1:end-1)==center(2));
-    poly = [circshift(poly(:, 1:end-1), -(find(is_center)-1),2), point_center];
-    outer_ring = poly(:, 2:end-1);
-end
-
-%% fast merge test
-dist_merge = sum((bsxfun(@minus,poly(:, 2:end-1), point_center).^2)*tan(phi), 1);
-dist_merge_mins = arrayfun(@(p1, p2) min([p1, p2]), dist_merge(1:end-1), dist_merge(2:end));
-length_edges = sum((poly(:, 2:end-2)-poly(:, 3:end-1)).^2, 1);
-% length_edges_min = min(length_edges);
-
-is_mergeable = dist_merge_mins > length_edges;
-ids_to_merge = find(is_mergeable);
-% id from -> id to
-merge_list = zeros(numel(ids_to_merge), 2); 
-%%
-for id = ids_to_merge
-    if id <= numel(is_mergeable)/2
-        merge_list
+    is_center = (poly(1,1:end-1)==point_center(1))&(poly(2,1:end-1)==point_center(2));
+    if any(is_center)
+         id_center = find(is_center);
+    else
+        dist_center = sum(bsxfun(@minus, poly(:,1:end-1), point_center).^2,1);
+        [~, id_center] = min(dist_center);
     end
-end
+    poly = [circshift(poly(:, 1:end-1), -(id_center-1),2), point_center];
+% else
+%     point_center = poly(:,1);
+% end
+%%%
+outer_ring = poly(:, 2:end-1);
+id_start = 1;
+id_next = 2;
 
+id_end = size(outer_ring, 2);
+id_previous = id_end -1;
 
-% if dist_center_max 
-
+ids_outer_ring = [id_start, id_end];
+merge_dists = sum((bsxfun(@minus, outer_ring, point_center).^2), 1).*tan(phi);
+is_all_merged = false;
 %%
+while ~is_all_merged 
+    %%
+    merge_dist_min_forward = min(merge_dists([id_start, id_next]));
+    merge_dist_min_backward = min(merge_dists([id_previous, id_end]));
+    
+    edge_length_forward = sum((outer_ring(:,id_start)-outer_ring(:, id_next)).^2, 1);
+    edge_length_backward = sum((outer_ring(:,id_previous)-outer_ring(:, id_end)).^2, 1);
 
-num_points = size(poly_open, 2);
-
-id_start = 2;
-id_end = num_points-1;
-while id_start < id_end
-    %% forward
-    if point
-
-
+    is_mergeable_forward = merge_dist_min_forward > edge_length_forward;
+    is_mergeable_backward = merge_dist_min_backward > edge_length_backward;
+    
+    %%% check if merge point is valid
+%     cla,    Environment.draw(environment, false);
+    is_inside_foreward = true;
+    if is_mergeable_forward
+    poly_forward = [point_center, outer_ring(:,[id_start, id_next+1]), point_center];
+    is_inside_foreward = binpolygon(outer_ring(:,id_next), poly_forward);
+    mb.drawPoint(outer_ring(:,id_next), 'color', 'g');
+    mb.drawPolygon(poly_forward, 'color', 'm');
+    end
+    is_inside_backward = true;
+    if is_mergeable_backward
+    poly_backward = [point_center, outer_ring(:,[id_previous-1, id_end]), point_center];
+    is_inside_backward = binpolygon(outer_ring(:,id_previous), poly_backward);
+        mb.drawPoint(outer_ring(:,id_previous), 'color', 'r');
+    mb.drawPolygon(poly_backward, 'color', 'b');
+    end
+%%%   
+    if id_next < id_previous
+        % pt must be added
+        if is_inside_foreward
+            ids_outer_ring = [ids_outer_ring, id_next];
+            id_start = id_next;
+        end
+        if is_inside_backward
+            ids_outer_ring = [ids_outer_ring, id_previous];
+            id_end = id_previous;
+        end
+        id_next = id_next + 1;
+        id_previous = id_previous - 1;
+    elseif id_next == id_previous
+        if is_inside_foreward || is_inside_backward
+            ids_outer_ring = [ids_outer_ring, id_next];
+        end
+        is_all_merged = true;
+    end
+    % last index
+    if id_next > id_previous
+        if is_inside_backward && is_inside_foreward
+            is_all_merged = true;
+        elseif is_mergeable_forward
+            id_previous = id_next;
+        else%if is_mergeable_backward
+            id_next = id_previous;
+        end
+%         is_all_merged = true;
+    end
+    fprintf(1, 'ids=%d idn=%d idp=%d ide=%d merged=%d\n',  [id_start, id_next, id_previous, id_end, is_all_merged]);
+    fprintf(1, '%d ', ids_outer_ring);
+    fprintf(1, '\n');
+end
+ids_outer_ring_sorted = sort(ids_outer_ring);
+poly = [point_center, outer_ring(:, ids_outer_ring_sorted), point_center];
 
 return;
-%% TEST with POly to merge
-phi = deg2rad(0.06);
+%% TEST with equal number of ring points to merge
+phi = 0.06;
 poly = int64([ 344, 3844, 3844, 3464, 344, 344 ; 4351, 1200, 1587, 1587, 4764, 4351 ;  ]);
 center = [ 3844 ; 1200 ; 1.5708 ;  ];
+%%
+poly_m = mb.mergePolygonPointsAngularDist(poly, phi, center);
 
+mb.drawPolygon(poly, 'color', 'r');
+mb.drawPoint(poly, 'color', 'r', 'marker', '*');
+mb.drawPolygon(poly_m, 'color', 'g');
+mb.drawPoint(poly_m, 'color', 'g');
+
+%%
+phi = 0.06;
+poly = int64([ 344, 3844, 3844, 3464, 344, 344, 344 ; 4351, 1200, 1587, 1587, 4764, 4666, 4351 ;  ]);
+center = [ 3844 ; 1200 ; 1.5708 ;  ];
+%%
+poly_m = mb.mergePolygonPointsAngularDist(poly, phi, center);
+
+mb.drawPolygon(poly, 'color', 'r');
+mb.drawPoint(poly, 'color', 'r', 'marker', '*');
+mb.drawPolygon(poly_m, 'color', 'g');
+mb.drawPoint(poly_m, 'color', 'g');
 
 %% TEST
 clear variables;
@@ -65,20 +133,53 @@ workspace_positions = Discretization.Workspace.iterative( environment, options )
 % options = config;
 %%%
 % for npts = randi(800, 1, 20)
-npts = 100;
-options.sensorspace.poses.additional = npts;
+% npts = 100;
+options.sensorspace.poses.additional = 0;
 %%%
 cla
-Environment.draw(environment, false); 
+Environment.draw(environment, false);
+environment.obstacles{end+1} = environment.mountable;
+environment.mountable = {};
+options.sensorspace.resolution.angular = deg2rad(5);
 %%%
 [sensor_poses, vfovs, vm] = Discretization.Sensorspace.iterative(environment, workspace_positions, options);
 
-Discretization.Sensorspace.draw(sensor_poses);
-cellfun(@(p) mb.drawPoint(p{1}{1}(:,2), 'color', 'g'), vfovs)
+% Discretization.Sensorspace.draw(sensor_poses);
+% cellfun(@(p) mb.drawPoint(p{1}{1}(:,2), 'color', 'g'), vfovs)
 % Discretization.Sensorspace.draw(sensor_poses_mountables, 'g');
 % Discretization.Sensorspace.draw(sensor_poses_initial_in, 'r');
 % disp(npts);
 %%
-phi = deg2rad(0.1);
-poly = int64([ 3765, 344, 344, 740, 1057, 1180, 2004, 2804, 3464, 3464, 3844, 3844, 3765, 3765 ; 5231, 8312, 2398, 2398, 1354, 1362, 1512, 1758, 2052, 2577, 2577, 3490, 3599, 5231 ;  ]);
-center = [ 344 ; 8312 ; 4.71239 ;  ];
+for id_vfov = 1:numel(vfovs)
+    %%
+    phi = 0.06;
+    center = sensor_poses(:, id_vfov);
+    poly = vfovs{id_vfov}{1}{1};
+    poly_m = mb.mergePolygonPointsAngularDist(poly, phi, center);
+    cla;
+   Environment.draw(environment, false); 
+mb.drawPolygon(poly, 'color', 'r');
+mb.drawPoint(poly, 'color', 'r', 'marker', '*');
+mb.drawPolygon(poly_m, 'color', 'g');
+mb.drawPoint(poly_m, 'color', 'g');
+disp(id_vfov);
+pause;
+end
+
+
+%%%
+%% fast merge test
+is_mergeable = true;
+dist_merge = sum((bsxfun(@minus, outer_ring, point_center).^2), 1).*tan(phi);
+for id_edge = 1:size(outer_ring, 2)-1
+    
+dist_merge_mins = arrayfun(@(p1, p2) min([p1, p2]), dist_merge(1:end-1), dist_merge(2:end));
+length_edges = sum((outer_ring(:,1:end-1)-outer_ring(:, 2:end)).^2, 1);
+% length_edges_min = min(length_edges);
+
+is_mergeable = dist_merge_mins > length_edges;
+% fill to move merged points to right location
+is_mergeable = [is_mergeable(1:ceil(end/2)), is_mergeable(ceil(end/2):end)];
+%%
+outer_ring = outer_ring(:,~is_mergeable);
+end
