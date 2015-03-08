7ce7c56fd81376301c1fcf6fd1d7bec680afba7d
NicolajKirchhof <nick@zuehlke.com>
2015-03-08 22:38:33 +0100

    Updated merge points for cmcqm

--- Src/Matlab/geom2d.addons/+mb/mergePoints.m
+++ Src/Matlab/geom2d.addons/+mb/mergePoints.m
@@ -5,15 +5,21 @@ is_cell = iscell(gpoly);
 if ~is_cell
     gpoly = {gpoly};
 end
-    
+
 
 fun_edge_lengths = cellfun(@(x) edgeLength([x circshift(x, -1, 1)]) > dist, gpoly, 'uniformoutput', false);
 gpoly = cellfun(@(x, flt) x(flt, :), gpoly, fun_edge_lengths, 'uniformoutput', false);
+gpoly = gpoly(~cellfun(@isempty, gpoly));
+gpoly = gpoly(cellfun(@(p) size(p, 1)>2, gpoly));
 
-
-if ~is_cell
-    gpoly = gpoly{1};
+if isempty(gpoly)
+    bpoly=gpoly;
+    
+else
+    
+    if ~is_cell
+        gpoly = gpoly{1};
+    end
+    
+    bpoly = mb.visilibity2boost(gpoly);
 end
-
-bpoly = mb.visilibity2boost(gpoly);
-
