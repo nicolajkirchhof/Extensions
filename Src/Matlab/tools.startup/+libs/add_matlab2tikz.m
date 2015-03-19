7f4feb9b03dc77b84c4020917d3d7d574864090e
NicolajKirchhof <nick@zuehlke.com>
2015-03-19 01:44:41 +0100

    updated matlab2tikz to version 0.6.0

--- Src/Matlab/tools.startup/+libs/add_matlab2tikz.m
+++ Src/Matlab/tools.startup/+libs/add_matlab2tikz.m
@@ -1,2 +1,2 @@
-addpath([libs.dir_extern '/matlab2tikz_0.4.0/src']);
-addpath([libs.dir_extern '/matlab2tikz_0.4.0/helpers']);
+addpath([libs.dir_extern '/matlab2tikz_0.6.0/src']);
+% addpath([libs.dir_extern '/matlab2tikz_0.6.0/helpers']);
