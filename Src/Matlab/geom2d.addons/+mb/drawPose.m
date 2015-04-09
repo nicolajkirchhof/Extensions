18a99da786f0e2ab1c59bc8f59ff6aa327f60fee
NicolajKirchhof <nicolaj.kirchhof@zuehlke.com>
2015-04-09 03:36:17 +0200

    Some updates on caluclations

--- Src/Matlab/geom2d.addons/+mb/drawPose.m
+++ Src/Matlab/geom2d.addons/+mb/drawPose.m
@@ -1,12 +1,17 @@
 function h = drawPose(p,varargin)
 % draws a pose x, y, phi on the screen
-length = max([diff(xlim) diff(ylim)])*0.1;
+if nargin < 2 || isstr(varargin{1})
+    length = max([diff(xlim) diff(ylim)])*0.1;
+else
+    length = varargin{1};
+    varargin = varargin(2:end);
+end
 rays = createRay(p(1:2,:)', p(3,:)');
 rays(:,3:4) = bsxfun(@plus, rays(:,1:2), rays(:,3:4)*length);
 
-if nargout > 0
-    h = [drawEdge(rays, varargin{:}); drawPoint(p(1:2,:)', 'marker', 'o', varargin{:})];
-else
-    drawEdge(rays, varargin{:});
-    drawPoint(p(1:2,:)', 'marker', 'o', varargin{:})
-end
+% if nargout > 0
+%     h = [drawEdge(rays, varargin{:}); drawPoint(p(1:2,:)', 'marker', 'o', varargin{:})];
+% else
+    h = drawEdge(rays, varargin{:}, 'marker', 'none');
+    h = [h; drawPoint(p(1:2,:)', 'marker', 'o', varargin{:}, 'linestyle', 'none')];
+% endk
